9366d2eb524154591418c634459b63807e2925eb
Manoj Krishna <y.manojkrishna@gmail.com>
2012-05-30 12:21:31 +0530

    adding all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Funtime::Application.config.secret_token = 'afedf7e2606c5107fc775d3ed4be2e444e564c2408d618bc23133c7f52381889c724d37c1536a31c8cb1a9e34f8c6161af62b2a8e1e7fcf4f2a422eba3bfa419'
